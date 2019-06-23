d451338bfd4ccf491fbd0a4054ccbc141d781e2b
memory-agape <memory.style.serv@gmail.com>
2019-06-24 01:16:16 +0900

    Update

--- src/magnolia/Client/Camera.php
+++ src/magnolia/Client/Camera.php
@@ -2,7 +2,9 @@
 namespace Magnolia\Client;
 
 use Magnolia\Contract\ClientInterface;
+use Magnolia\Stream\Stream;
 use Monolog\Logger;
+use Swoole\Coroutine\Channel;
 
 final class Camera extends AbstractClient implements ClientInterface
 {
@@ -13,6 +15,12 @@ final class Camera extends AbstractClient implements ClientInterface
 
     public function start(): void
     {
+        /**
+         * @var Channel $channel
+         */
+        $channel = $this->channels[\Magnolia\Server\StreamingPipeline::class];
+
+        $promiseCounter = new \Swoole\Atomic(0);
         while (true) {
             while ($sizePacket = $this->client->read(4)) {
                 $size = current(unpack('L', $sizePacket));
@@ -20,6 +28,36 @@ final class Camera extends AbstractClient implements ClientInterface
                     continue;
                 }
                 $packet = $this->client->read($size);
+
+                // Send image data to clients in coroutine
+                $clients = [];
+
+                // initialize to default value
+                $promiseCounter->set(0);
+
+                $targetClients = $channel->length();
+                while (!$channel->isEmpty()) {
+                    $client = $channel->pop();
+
+                    /**
+                     * @var Stream $client
+                     */
+                    go(function () use ($client, $packet, $promiseCounter) {
+                        // send packets
+                        $client->write($packet);
+
+                        // add counter
+                        $promiseCounter->add(1);
+                    });
+                }
+
+                // Waiting proceeded.
+                while($promiseCounter->get() < $targetClients);
+
+                // re-push clients
+                foreach ($clients as $client) {
+                    $channel->push($client);
+                }
             }
         }
     }
