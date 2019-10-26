c9afb967c016da30361780588d5a6b2d70a65891
memory-agape <memory.style.serv@gmail.com>
2019-10-27 00:46:07 +0900

    Update

--- update_certificate.php
+++ update_certificate.php
@@ -7,6 +7,13 @@ $env->load();
 $based_directory = $_ENV['LILY_CERTIFICATE_BASED_DIRECTORY'] ?? null;
 $target = $_ENV['LILY_NGINX_TARGET'] ?? null;
 
+exec('whoami', $output);
+
+if (($output[0] ?? null) !== 'root') {
+    echo "You need to login with root user.\n";
+    exit(1);
+}
+
 if ($based_directory === null || $target === null) {
     echo "Please set envs LILY_CERTIFICATE_BASED_DIRECTORY and LILY_NGINX_TARGET.\n";
     exit(1);
